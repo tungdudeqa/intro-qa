cbb1b8f2067aaf990ab703d5f20a3f9806cab5a8
tungdudeqa <kmanu@sertiscorp.com>
2025-06-04 17:26:07 +0700

    config

--- conftest.py
+++ conftest.py
@@ -1,4 +1,6 @@
 import os
+import pytest
+from playwright.sync_api import sync_playwright
 
 def pytest_logger_config(logger_config):
     logger_config.add_loggers(['logger'], stdout_level='info')
@@ -6,3 +8,17 @@ def pytest_logger_config(logger_config):
 
 def pytest_logger_logdirlink(config):
     return os.path.join(os.path.dirname(__file__), 'tlogs')
+
+@pytest.fixture(scope="function")
+def browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        yield browser
+        browser.close()
+
+@pytest.fixture(scope="function")
+def page(browser):
+    context = browser.new_context()
+    page = context.new_page()
+    yield page
+    context.close()
